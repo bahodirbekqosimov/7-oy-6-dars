30971e1f0c99ac83e10e2f943d5d18c40f036427
Bahodirbek <bahodirbek20090405@gmail.com>
2026-01-10 00:15:51 +0500

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Note(models.Model):
+    user = models.ForeignKey(AbstractUser, on_delete=models.CASCADE, related_name="note")
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Note
 
-# Create your views here.
+class NoteCreateView(CreateAPIView):
+    queryset = Note.objects.filter(is_active = True)
+    # serializer_class = 
+    
+
+class NoteDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.filter(is_active = True)
+    
+    
+    
+    def perform_destroy(self, instance:Note):
+        instance.is_active = False
+        instance.save()
+        
+        
